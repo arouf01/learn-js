6e02c2d2babb0e9c088dd584ac5a57a866a076f3
arouf01 <arouf10022@gmail.com>
2025-06-28 10:33:37 +0600

    Date: 28/06/2025, Added: 4.Basic Operators (1. Arithmetic Operators)

---
+++ 4. Basic Operators/1. Arithmetic Operators/ArithmeticOperators.js
@@ -0,0 +1,37 @@
+// Date: 28/06/2025
+
+/*
+1. Addition
+2. Subtruction
+3. Multiplication
+4. Divisioin
+5. Modulas
+6. Exponentiation
+*/
+
+const num1 = 5;
+const num2 = 2;
+
+// 1. Addition
+const sum = num1 + num2;
+console.log(sum);
+
+// 2. Subtruction
+const sub = num1 - num2;
+console.log(sub);
+
+// 3. Multiplication
+const multiplication = num1 * num2;
+console.log(multiplication);
+
+// 4. Divisioin
+const divisioin = num1 / num2;
+console.log(divisioin);
+
+// 5. Modulas
+const modulas = num1 % num2;
+console.log(modulas);
+
+// 6. Exponentiation
+const exponentiation = num1 ** num2;
+console.log(exponentiation);
