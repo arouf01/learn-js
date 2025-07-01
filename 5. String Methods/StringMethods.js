6af18fcee3f3e04a603989221d403abf5d46c069
arouf01 <arouf10022@gmail.com>
2025-07-01 20:23:47 +0600

    Date: 01/07/2025, Added: 5. String Methods

---
+++ 5. String Methods/StringMethods.js
@@ -0,0 +1,23 @@
+// Date: 01/07/2025
+
+// 1. length
+let count = "Hey, there!";
+console.log(count.length);
+
+// 2. toUpperCase()
+let sample = "abcdefgh";
+console.log(sample.toUpperCase());
+
+// 3. toLowerCase()
+let demo = "AbcdEFGh";
+console.log(demo.toLowerCase());
+
+// 4. slice()
+let example = "Wait a minute... Who are you ?";
+console.log(example.slice(17, 30));
+// in slice, we can also use the negative value for getting from the last char
+console.log(example.slice(17, -1));
+
+// 5. substring()
+let cut = "Ok! Bye...";
+console.log(cut.substring(4, 10)); // Can't use the negative value here
--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Welcome to my JavaScript learning repository! This is where I document my progre
 - Variables and Constants
 - Data Types
 - Basic Operators
+- String Methods
 
 *
 
