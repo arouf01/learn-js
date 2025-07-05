277647137086828d9f5b2fe350f719bfa60d11d1
arouf01 <arouf10022@gmail.com>
2025-07-05 18:38:08 +0600

    Date: 05/07/2025, Added: 9. Conditional Statements

---
+++ 9. Conditional Statements/ConditionalStatements.js
@@ -0,0 +1,65 @@
+// Date: 05/07/2025
+/*
+In JavaScript we have the following conditional statements:
+
+Use if to specify a block of code to be executed, if a specified condition is true
+Use else to specify a block of code to be executed, if the same condition is false
+Use else if to specify a new condition to test, if the first condition is false
+Use switch to specify many alternative blocks of code to be executed
+*/
+
+/*
+The if Statement
+// Use the if statement to specify a block of JavaScript code to be executed if a condition is true.
+Syntax
+*/
+if (condition) {
+  //  block of code to be executed if the condition is true
+}
+
+if (hour < 18) {
+  greeting = "Good day";
+}
+
+/*
+The else Statement
+Use the else statement to specify a block of code to be executed if the condition is false.
+
+*/
+if (condition) {
+  //  block of code to be executed if the condition is true
+} else {
+  //  block of code to be executed if the condition is false
+}
+
+// If the hour is less than 18, create a "Good day" greeting, otherwise "Good evening":
+
+if (hour < 18) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+/*
+The else if Statement
+Use the else if statement to specify a new condition if the first condition is false.
+
+Syntax:
+*/
+
+if (condition1) {
+  //  block of code to be executed if condition1 is true
+} else if (condition2) {
+  //  block of code to be executed if the condition1 is false and condition2 is true
+} else {
+  //  block of code to be executed if the condition1 is false and condition2 is false
+}
+// If time is less than 10:00, create a "Good morning" greeting, if not, but time is less than 20:00, create a "Good day" greeting, otherwise a "Good evening":
+
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
--- README.md
+++ README.md
@@ -18,6 +18,7 @@ Welcome to my JavaScript learning repository! This is where I document my progre
 - Math Objects
 - Number Methods
 - Boolean Values
+- Conditional Statements
 
 *
 
